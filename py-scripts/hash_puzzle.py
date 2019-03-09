165bbcc471f514369723911203c99ba9abaf9d64
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-03-09 18:48:46 +0100

    improved incremental reporting

--- py-scripts/hash_puzzle.py
+++ py-scripts/hash_puzzle.py
@@ -11,10 +11,10 @@
 import hashlib
 import time
 
-msg = input('insert string: ')
+msg = input('insert string (return for "Hello, world!"): ')
 if msg == "": msg = "Hello, world!"
 
-zerostr = input('n of zeros: ')
+zerostr = input('n of zeros (return for 4 zeros): ')
 if zerostr == "":
     zeros = 4
 else:
@@ -24,32 +24,35 @@ assert (zeros > 0), "the number of zeros to look for must be greater than zero"
 print(f"\nstring is: {msg}")
 print(f"looking for {zeros} zeros")
 
-start = time.time()
-
 # n[i] is used to count the results starting with i+1 zeros
-n = [0 for x in range(zeros)]
-maxEval = pow(16,zeros+1)
+n = []
+nonce = 0
+maxEval = pow(16, zeros+1)
 i = 0
-while (n[zeros-1] == 0 and i < maxEval):
+j = 0
+start = time.time()
+while (i < maxEval and nonce == 0):
     string = msg+str(i)
     hashValue = hashlib.sha256(string.encode()).hexdigest()
-    for j in range (0, zeros):
-        if hashValue[j] != "0":
-            break
-        n[j] += 1
-        if n[zeros-1] == 1: nonce = i
+    while (hashValue[j] == "0"):
+        if j < len(n):
+            n[j] += 1
+        else:
+            n.append(1)
+            elapsed = time.time() - start
+            report = f"{j+1} zeros found {n}"
+            if elapsed > 0:
+                report += f" in {round(elapsed)} seconds at {round(i/elapsed)} hash/s"
+            print(report)
+            if j == zeros-1:
+                nonce = i
+        j += 1
+    j = 0
     i += 1
 
-elapsed = round(time.time() - start)
-
-print(f"\nperformed evaluations: {i} / {maxEval}")
-print(f"elapsed time         : {elapsed} seconds")
-print(f"\nzeros found: {n}")
-
 if (n[zeros-1] == 1):
-    print("nonce      :", nonce)
-    string = msg+str(nonce)
-    print("\n"+string)
-    print(hashlib.sha256(string.encode()).hexdigest())
+    print("nonce:", nonce)
+    print(string)
+    print(hashValue)
 else:
     print("nonce not found")
