b5458bfb2d5ec0d7f2428ce771f8a8dafe2fbe3d
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-03-09 22:10:06 +0100

    improved reporting

--- py-scripts/hash_puzzle.py
+++ py-scripts/hash_puzzle.py
@@ -26,23 +26,25 @@ print(f"looking for {zeros} zeros")
 
 # n[i] is used to count the results starting with i+1 zeros
 n = []
-nonce = 0
 maxEval = pow(16, zeros+1)
-i = 0
-j = 0
+i = j = nonce = 0
 start = time.time()
-while (i < maxEval and nonce == 0):
+while i < maxEval and nonce == 0:
     string = msg+str(i)
     hashValue = hashlib.sha256(string.encode()).hexdigest()
-    while (hashValue[j] == "0"):
+    while hashValue[j] == "0":
         if j < len(n):
             n[j] += 1
         else:
             n.append(1)
             elapsed = time.time() - start
             report = f"{j+1} zeros found {n}"
-            if elapsed > 0:
+            if 0 < elapsed <= 600:
                 report += f" in {round(elapsed)} seconds at {round(i/elapsed)} hash/s"
+            elif 600 < elapsed <= 36000:
+                report += f" in {round(elapsed/60)} minutes at {round(i/elapsed)} hash/s"
+            elif 36000 < elapsed:
+                report += f" in {round(elapsed/3600)} hours at {round(i/elapsed)} hash/s"
             print(report)
             if j == zeros-1:
                 nonce = i
@@ -50,7 +52,7 @@ while (i < maxEval and nonce == 0):
     j = 0
     i += 1
 
-if (n[zeros-1] == 1):
+if n[zeros-1] == 1:
     print("nonce:", nonce)
     print(string)
     print(hashValue)
