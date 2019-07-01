1867d2aacbf559b06d48fdf085d61df8b4f4b6e8
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-07-01 23:14:45 +0200

    added  -uacomment, disable mainnet wallet

--- regtest-lab/windowsbat/testnet3-18333.bat
+++ regtest-lab/windowsbat/testnet3-18333.bat
@@ -1,2 +1,2 @@
-start bin\bitcoin-qt.exe -testnet -server -rpcallowip=127.0.0.1 -addresstype=bech32 -walletrbf
+start bin\bitcoin-qt.exe -testnet -server -rpcallowip=127.0.0.1 -addresstype=bech32 -walletrbf -uacomment=portable
 ::start cmd /k cd bin
