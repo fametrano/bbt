d936c0b20badc835f24cd92d3bb6f3546fa0da42
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-11-27 18:59:38 +0100

    updated

--- regtest-lab/windowsbat/mainnet-8333.bat
+++ regtest-lab/windowsbat/mainnet-8333.bat
@@ -1,2 +1,2 @@
-start bin\bitcoin-qt.exe -server -rpcallowip=127.0.0.1 -addresstype=bech32 -walletrbf
+start bin\bitcoin-qt.exe -server -rpcallowip=127.0.0.1 -addresstype=bech32 -walletrbf -uacomment=bat -disablewallet
 ::start cmd /k cd bin
