a42a1cc24d6fafce59569a6db1a4b8765b15c22c
Ferdinando Ametrano <ferdinando@ametrano.net>
2020-05-05 02:48:34 +0200

    renamed files

--- py-scripts/det_key_seq_type1.py
+++ py-scripts/det_key_seq_type1.py
@@ -8,7 +8,7 @@
 # No part of btclib including this file, may be copied, modified, propagated,
 # or distributed except according to the terms contained in the LICENSE file.
 
-""" Deterministic Wallet (Type-1)"""
+""" Deterministic Key Sequence (Type-1)"""
 
 import secrets
 from hashlib import sha256 as hf
