5d7383908916c44571aa2a10aa2a5ca8b5750e38
fametrano <ferdinando@ametrano.net>
2020-11-22 23:44:20 +0100

    updated

--- py-scripts/speedup_batchval.py
+++ py-scripts/speedup_batchval.py
@@ -14,7 +14,7 @@ from hashlib import sha256 as hf
 
 from btclib.curve import mult
 from btclib.curve import secp256k1 as ec
-from btclib.ssa import batch_verify, sign, verify
+from btclib.ssa import _batch_verify, _sign, _verify
 
 random.seed(42)
 
@@ -22,10 +22,10 @@ hsize = hf().digest_size
 hlen = hsize * 8
 
 # n = 1 loops forever and does not really test batch verify
-n_sig = [2, 4, 8, 16, 32, 64, 128]
+n_sig = [4, 8, 16, 32, 64, 128, 256, 512]
 m = [random.getrandbits(hlen).to_bytes(hsize, "big") for _ in range(max(n_sig))]
 q = [random.getrandbits(ec.nlen) % ec.n for _ in m]
-sig = [sign(msg, qq) for msg, qq in zip(m, q)]
+sig = [_sign(msg, qq) for msg, qq in zip(m, q)]
 Q = [mult(qq, ec.G)[0] for qq in q]
 
 for n in n_sig:
@@ -33,12 +33,15 @@ for n in n_sig:
     # no batch
     start = time.time()
     for j in range(n):
-        assert verify(m[j], Q[j], sig[j])
+        assert _verify(m[j], Q[j], sig[j])
     elapsed1 = time.time() - start
 
     # batch
+    ms = m[:n]
+    Qs = Q[:n]
+    sigs = sig[:n]
     start = time.time()
-    assert batch_verify(m[:n], Q[:n], sig[:n]), n
+    assert _batch_verify(ms, Qs, sigs), n
     elapsed2 = time.time() - start
 
-print(n, elapsed2 / elapsed1)
+    print(n, elapsed2 / elapsed1)
